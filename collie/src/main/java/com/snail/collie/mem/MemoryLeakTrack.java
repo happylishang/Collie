beb58f115433d3b234889af56776374b04b24e25
李尚 <happylishang@163.com>
2020-08-21 13:14:43 +0800

    weakhashmap简化下

--- collie/src/main/java/com/snail/collie/mem/MemoryLeakTrack.java
+++ collie/src/main/java/com/snail/collie/mem/MemoryLeakTrack.java
@@ -29,71 +29,59 @@ public class MemoryLeakTrack implements ITracker {
 
     private static volatile MemoryLeakTrack sInstance = null;
 
-    private MemoryLeakTrack(){}
+    private MemoryLeakTrack() {
+    }
 
     public static MemoryLeakTrack getInstance() {
         if (sInstance == null) {
             synchronized (MemoryLeakTrack.class) {
-                 if (sInstance == null) {
+                if (sInstance == null) {
                     sInstance = new MemoryLeakTrack();
-                 }
+                }
             }
         }
         return sInstance;
     }
-    private HashSet<WeakReference<Activity>> weakReferences = new HashSet<>();
-    private ReferenceQueue<Activity> mReferenceQueue = new ReferenceQueue<>();
+
     private Handler mHandler = new Handler(CollieHandlerThread.getInstance().getHandlerThread().getLooper());
-    private WeakHashMap<Activity ,String> mActivityStringWeakHashMap=new WeakHashMap<>();
+    private WeakHashMap<Activity, String> mActivityStringWeakHashMap = new WeakHashMap<>();
     private SimpleActivityLifecycleCallbacks mSimpleActivityLifecycleCallbacks = new SimpleActivityLifecycleCallbacks() {
         @Override
         public void onActivityDestroyed(@NonNull Activity activity) {
             super.onActivityDestroyed(activity);
-            weakReferences.add(new WeakReference<>(activity, mReferenceQueue));
-            mActivityStringWeakHashMap.put(activity,activity.getClass().getSimpleName());
+            mActivityStringWeakHashMap.put(activity, activity.getClass().getSimpleName());
         }
 
         @Override
         public void onActivityStopped(@NonNull final Activity activity) {
             super.onActivityStopped(activity);
+            //  退后台，GC 找LeakActivity
             if (ActivityStack.getInstance().isInBackGround()) {
-                Runtime.getRuntime().gc();
-                System.gc();
-                SystemClock.sleep(100);
-                System.runFinalization();
-                mHandler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Log.v("MemoryLeakTrack", "start");
-                        try {
-//                            Reference k;
-//                            while ((k = mReferenceQueue.poll()) != null) {
-//                                weakReferences.remove(k);
-//                            }
-//                            for (WeakReference<Activity> item : weakReferences) {
-//                                Log.v("MemoryLeakTrack", "" + (item.get()==null?"d":item.get()));
-//                            }
-
-//                            WeakHashMap<Activity ,String>
-
-
-                            for (Map.Entry<Activity ,String> activityStringEntry : mActivityStringWeakHashMap.entrySet()) {
-                                Log.v("MemoryLeakTrack", "" +  activityStringEntry.getKey());
-                            }
-
-
-                        } catch (Exception e) {
-                            Log.v("MemoryLeakTrack", "3");
-                        }
-                    }
-                }, 5000);
+                return;
             }
+            Runtime.getRuntime().gc();
+            SystemClock.sleep(100);
+            System.runFinalization();
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        if (!ActivityStack.getInstance().isInBackGround()) {
+                            return;
+                        }
+                        for (Map.Entry<Activity, String> activityStringEntry : mActivityStringWeakHashMap.entrySet()) {
+                            Log.v("MemoryLeakTrack", "" + activityStringEntry.getKey());
+                        }
+                    } catch (Exception ignored) {
+                    }
+                }
+            }, 5000);
         }
     };
 
     @Override
     public void destroy() {
-
+        Collie.getInstance().removeActivityLifecycleCallbacks(mSimpleActivityLifecycleCallbacks);
     }
 
     @Override
@@ -106,7 +94,7 @@ public class MemoryLeakTrack implements ITracker {
 
     }
 
-    private void findLeakActivity() {
+    public interface OnMemoryLeakListener {
 
     }
 }
